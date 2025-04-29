93361f8141da43b6859e557be47346b48bd7efe0
MLGM2001 <canonmauricio63@gmail.com>
2025-04-29 02:27:24 -0400

    .

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,4 +1,5 @@
-const API_URL = 'http://localhost:4000/api/auth';
+// const API_URL = 'http://localhost:4000/api/auth';
+const API_URL = 'https://sf1-backend.onrender.com/api/auth';
 
 export const register = async (userData) => {
   const res = await fetch(`${API_URL}/register`, {
